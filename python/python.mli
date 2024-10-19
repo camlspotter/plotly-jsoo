e240a45baad864bf31364064564f1c2356dfaff0
Adrien Nader <adrien.nader@canonical.com>
2024-10-19 23:18:17 +0200

    (WIP) Add colorscale and labels.

--- python/python.mli
+++ python/python.mli
@@ -2,7 +2,7 @@ type _ t = private Py.Object.t
 
 type figure
 
-val of_figure : Plotly.Figure.t -> figure t
+val of_figure : ?z:float array -> ?text:string array -> Plotly.Figure.t -> figure t
 
 val show : ?renderer:string -> figure t -> unit
 val write_image : figure t -> string -> unit
